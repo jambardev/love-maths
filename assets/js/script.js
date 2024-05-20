d8da5ed9ad6f84401eadf2bb91a3fb5a04faf40a
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-05-20 21:33:47 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } 
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -90,7 +92,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     }  else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } 
+    }  else if (operator === "÷") {
+        return [operand1 / operand2, "division"];
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -131,7 +135,8 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 }
 
-function displayDivideQuestion(operand1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1*operand2;
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "÷";
